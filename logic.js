78adb2d55e2d4fcf02010a88ca4a36549ee822a8
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-15 13:14:27 -0400

    stylesheet

--- logic.js
+++ logic.js
@@ -14,29 +14,23 @@ var quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_d
 d3.json(quakesUrl, function(data) {
     console.log(data.features);
 
-    function rad(magn) {
-        return magn*2;
-    }
-
     function onEachFeature(feature, layer) { //add popups
         layer.bindPopup("<h3>" + feature.properties.place +
           "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
     };
     
-    // var quakeLayer = L.geoJSON(data.features, {
-    //     onEachFeature: onEachFeature
-    //     }).addTo(myMap); //create a new layer for quakes
-
-    // console.log(quakeLayer);
-    
+    var colormap = d3.scale.linear()
+            .domain([0, 3, 7])
+            .range(["blue", "orange", "red"]);
     
     L.geoJSON(data, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, {
                 radius: feature.properties.mag * 4,
-                // fillColor: "white",
-                color: "red",
-                fillOpacity: 0.8
+                fillColor: colormap(feature.properties.mag),
+                // color: "white",
+                fillOpacity: 1,
+                opacity: 0.5
                 
             })
         },
