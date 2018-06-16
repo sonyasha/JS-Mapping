405fb8c2a7d5bfe0c61d21c73248b66e335fd500
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-16 10:06:10 -0400

    fixing the order

--- logic.js
+++ logic.js
@@ -1,7 +1,84 @@
-// var access_token = token;
+var quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+var platesUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json';
+
+d3.json(quakesUrl, function(quakedata) {
+    getQuakes(quakedata);
+});
+
+function getQuakes(data) {        
+
+    //create quakes layer
+    function onEachFeature(feature, layer) { 
+        layer.bindPopup("<h3>" + feature.properties.place +
+          "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
+    };
+    var colormap = d3.scale.linear()
+    .domain([0, 5])
+    .range(["yellow", "red"]);
+
+    var quakesData = L.geoJSON(data, {
+
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+
+            radius: feature.properties.mag * 3,
+            fillColor: colormap(feature.properties.mag),
+            color: 'black',
+            fillOpacity: 1,
+            weight: 0.6
+            
+        })
+    },
+    onEachFeature: onEachFeature
+    });
+
+    //create quakes legend control
+    var quakesLegend = L.control({ position: "bottomright" });
+
+    quakesLegend.onAdd = function() {
+
+    var geojson = L.choropleth(data, {
+        valueProperty: "mag",
+        scale: ["yellow", "red"],
+        steps: 6,    
+    });
+
+    var div = L.DomUtil.create("div", "info legend");
+
+    var magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
+
+    var labelsColors = geojson.options.limits.map((lim, ind) => {
+        return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
+    })
+    var labelsLabels = geojson.options.limits.map((lim, ind) => {
+        return ('<li>'+ magnitudes[ind]+ '</li>')
+    })
+    
+    div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
+    return div;
+    };
+
+    getPlates(quakesData, quakesLegend);
+}
+
+//create plates layer
+function getPlates(quakes,legend) { 
+
+    d3.json(platesUrl, function(data) {
+      var plates = L.geoJSON(data, {
+        style: function (feature) {
+          return {
+            color: 'orange',
+            weight: 1,
+            fillColor: 'none'
+          };
+      }
+      });
+      renderMap(quakes, legend, plates);
+    });
+};
 
 function renderMap(quakes, legend, plates) {
-// function renderMap(getQuakes) {
 
     var satellTiles = L.tileLayer(
         "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
@@ -28,14 +105,28 @@ function renderMap(quakes, legend, plates) {
     var myMap = L.map("map", {
         center: [0, 0],
         zoom: 2,
-        layers: [satellTiles, quakes]
-      });
+        layers: [satellTiles, quakes],
+        timeDimension: true,
+        fullscreenControl: true,
+        timeDimensionOptions: {
+            autoPlay: true,
+            position: 'bottomleft',
+            playerOptions: {
+                transitionTime: 500,
+                loop: true,
+            },
+        },
+    });
+
+    
 
     L.control.layers(baseMaps, overLay, {
-        // collapsed: false
+        collapsed: false
       }).addTo(myMap);
     
     legend.addTo(myMap);
+    
+
 
     myMap.on('overlayadd', function(eventLayer){
         if (eventLayer.name === 'Quakes'){
@@ -52,89 +143,70 @@ function renderMap(quakes, legend, plates) {
 };
 
 
-var platesUrl = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json';
-var quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson';
 
 
-d3.json(quakesUrl, function(data) {
-    console.log(data.features);
-
-    function onEachFeature(feature, layer) { //add popups
-        layer.bindPopup("<h3>" + feature.properties.place +
-          "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
-    };
     
-    var colormap = d3.scale.linear()
-        .domain([0, 5])
-        .range(["yellow", "red"]);
+
     
-    quakesData = L.geoJSON(data, {
 
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
+    // function onEachFeature(feature, layer) { //add popups
+    //     layer.bindPopup("<h3>" + feature.properties.place +
+    //       "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
+    // };
+    
+    // var colormap = d3.scale.linear()
+    //     .domain([0, 5])
+    //     .range(["yellow", "red"]);
+    
+    // var quakesData = L.geoJSON(data, {
 
-                radius: feature.properties.mag * 3,
-                fillColor: colormap(feature.properties.mag),
-                color: 'black',
-                fillOpacity: 1,
-                weight: 0.6
+    //     pointToLayer: function (feature, latlng) {
+    //         return L.circleMarker(latlng, {
+
+    //             radius: feature.properties.mag * 3,
+    //             fillColor: colormap(feature.properties.mag),
+    //             color: 'black',
+    //             fillOpacity: 1,
+    //             weight: 0.6
                 
-            })
-        },
-        onEachFeature: onEachFeature
+    //         })
+    //     },
+    //     onEachFeature: onEachFeature
+    // });
+
+    // var quakesLegend = L.control({ position: "bottomright" });
+
+    // quakesLegend.onAdd = function() {
+
+    //     var geojson = L.choropleth(data, {
+    //         valueProperty: "mag",
+    //         scale: ["yellow", "red"],
+    //         steps: 6,    
+    //     });
+
+    //     var div = L.DomUtil.create("div", "info legend");
+
+    //     var magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
+
+    //     var labelsColors = geojson.options.limits.map((lim, ind) => {
+    //         return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
+    //     })
+    //     var labelsLabels = geojson.options.limits.map((lim, ind) => {
+    //         return ('<li>'+ magnitudes[ind]+ '</li>')
+    //     })
         
-    // })    
-    });
+    //     div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
+    //     return div;
+    // };
 
-    quakesLegend = L.control({ position: "bottomright" });
-
-    quakesLegend.onAdd = function() {
-
-        var geojson = L.choropleth(data, {
-            valueProperty: "mag",
-            scale: ["yellow", "red"],
-            steps: 6,    
-        });
-
-        var div = L.DomUtil.create("div", "info legend");
-
-        var magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
-
-        var labelsColors = geojson.options.limits.map((lim, ind) => {
-            return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
-        })
-        var labelsLabels = geojson.options.limits.map((lim, ind) => {
-            return ('<li>'+ magnitudes[ind]+ '</li>')
-        })
-        
-        div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
-        return div;
-    }
-
-
-    // quakesLegend.addTo(myMap);
-    // quakesData.addTo(myMap);
-    getPlates(quakesData, quakesLegend);
-    // renderMap(quakesData, quakesLegend)
+    // getPlates(quakesData, quakesLegend, tdWmsLayer);
+    // getPlates(quakesData, quakesLegend);
     
-    
-    });
-    
-    // return quakesData, quakesLegend
-// }
 
-function getPlates(q,l) {
 
-    d3.json(platesUrl, function(data) {
-      var plates = L.geoJSON(data, {
-        style: function (feature) {
-          return {
-            color: 'orange',
-            weight: 1,
-            fillColor: 'none'
-          };
-      }
-      });
-      renderMap(q, l, plates);
-    });
-  }
+
+
+
+
+
+
