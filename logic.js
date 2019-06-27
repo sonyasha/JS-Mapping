74746e09a7755b636f7148de0441d322f500c97f
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2019-06-27 16:13:48 -0400

    refactoring

--- logic.js
+++ logic.js
@@ -5,82 +5,223 @@ var groundURL = 'pk.eyJ1IjoiZ3J5YXp6eiIsImEiOiJjamozbTc1eWYwY2xzM3Bwb2U4bWoxMHZ6
 var floodURL = 'https://waterwatch.usgs.gov/webservices/realtime?format=json';
 
 
-d3.json(quakesUrl, getQuakes);
+const render = () => {
 
-function getQuakes(data) {        
-
-    //create quakes layer
-    function onEachFeature(feature, layer) { 
+    const onEachFeature = (feature, layer) => { 
         layer.bindPopup("<h3>" + feature.properties.place +
           "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
     };
-    var colormap = d3.scale.linear()
-    .domain([0, 5])
-    .range(["yellow", "red"]);
-
-    var quakesData = L.geoJSON(data, {
-
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-
-                radius: feature.properties.mag * 3,
-                fillColor: colormap(feature.properties.mag),
-                color: 'black',
-                fillOpacity: 1,
-                weight: 0.6
-                
-            })
-        },
-        onEachFeature: onEachFeature
-    });
-
-    //create quakes legend control
-    var quakesLegend = L.control({ position: "bottomright" });
-
-    quakesLegend.onAdd = function() {
-
-    var geojson = L.choropleth(data, {
-        valueProperty: "mag",
-        scale: ["yellow", "red"],
-        steps: 6,    
-    });
-
-    var div = L.DomUtil.create("div", "info legend");
-
-    var magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
-
-    var labelsColors = geojson.options.limits.map((lim, ind) => {
-        return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
-    })
-    var labelsLabels = geojson.options.limits.map((lim, ind) => {
-        return ('<li>'+ magnitudes[ind]+ '</li>')
-    })
     
-    div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
-    return div;
-    };
+    const colormap = d3.scale.linear()
+        .domain([0, 5])
+        .range(["yellow", "red"]);
 
-    getPlates(quakesData, quakesLegend);
+    let quakesLegend = L.control({ position: "bottomright" });
+    
+    Promise.all([
+        fetch(quakesUrl, {mode: 'cors'}).then(response => response.json()),
+        fetch(platesUrl, {mode: 'cors'}).then(response => response.json()),
+    ])
+    .then(data => {
+        const all_data = data
+        console.log(all_data)
+    
+        const plates = L.geoJSON(all_data[1], {
+            style: feature => {
+                return {
+                color: 'orange',
+                weight: 1,
+                fillColor: 'none'
+                };
+            }
+        });
+        console.log(plates);
+    
+        const quakesData = L.geoJSON(all_data[0], {
+    
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+    
+                    radius: feature.properties.mag * 3,
+                    fillColor: colormap(feature.properties.mag),
+                    color: 'black',
+                    fillOpacity: 1,
+                    weight: 0.6
+                    
+                })
+            },
+            onEachFeature: onEachFeature
+        });
+        console.log(quakesData);
+
+        quakesLegend.onAdd = () => {
+
+            let geojson = L.choropleth(all_data[0], {
+                valueProperty: "mag",
+                scale: ["yellow", "red"],
+                steps: 6,    
+            });
+            
+            let div = L.DomUtil.create("div", "info legend");
+            
+            let magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
+            
+            let labelsColors = geojson.options.limits.map((lim, ind) => {
+                return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
+            })
+            let labelsLabels = geojson.options.limits.map((lim, ind) => {
+                return ('<li>'+ magnitudes[ind]+ '</li>')
+            })
+            
+            div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
+            return div;
+        };
+    
+        renderMap(quakesData, quakesLegend, plates);
+        
+    })
+
 }
 
-//create plates layer
-function getPlates(quakes,legend) { 
+const renderMap = (quakes, legend, plates) => {
+        let satellTiles = L.tileLayer(
+            "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
+              "access_token=" + groundURL);
+        
+        let regTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?' +
+        "access_token=" + groundURL);
+    
+        let ligthTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?' +
+        "access_token=" + groundURL)
+    
+        let baseMaps = { 
+            "Satellite": satellTiles,
+            "Street": regTiles,
+            'Greyscale': ligthTiles
+        };
+        
+        let overLay = { 
+            'Quakes': quakes,
+            'Plates': plates,
+            // 'Explosions': volcs,
+            //'Water Stations': wcl,
+            //'Water Stations Heat': water
+        };
+    
+        let myMap = L.map("map", {
+            center: [0, 0],
+            zoom: 2,
+            layers: [satellTiles, quakes],
+            timeDimension: true,
+            fullscreenControl: true,
+            timeDimensionOptions: {
+                autoPlay: true,
+                position: 'bottomleft',
+                playerOptions: {
+                    transitionTime: 500,
+                    loop: true,
+                },
+            },
+        });
+    
+        L.control.layers(baseMaps, overLay, {
+            collapsed: false
+          }).addTo(myMap);
+        
+        legend.addTo(myMap);
+        
+        //disappearing legend
+        myMap.on('overlayadd', eventLayer => {
+            if (eventLayer.name === 'Quakes'){
+                myMap.addControl(legend);
+            } 
+        });
+        
+        myMap.on('overlayremove', eventLayer => {
+            if (eventLayer.name === 'Quakes'){
+                myMap.removeControl(legend);
+            } 
+        });
+    }
 
-    d3.json(platesUrl, function(data) {
-      var plates = L.geoJSON(data, {
-        style: function (feature) {
-          return {
-            color: 'orange',
-            weight: 1,
-            fillColor: 'none'
-          };
-        }
-      });
-    //   getVocanoes(quakes, legend, plates);
-    //getWater(quakes, legend, plates);
-    renderMap(quakes, legend, plates);
-    });
-};
+
+
+// function getQuakes(data) {        
+
+//     //create quakes layer
+//     function onEachFeature(feature, layer) { 
+//         layer.bindPopup("<h3>" + feature.properties.place +
+//           "</h3><hr><p>" + new Date(feature.properties.time) + ', magnitude ' + feature.properties.mag + "</p>");
+//     };
+//     var colormap = d3.scale.linear()
+//     .domain([0, 5])
+//     .range(["yellow", "red"]);
+//     console.log(colormap)
+
+//     var quakesData = L.geoJSON(data, {
+
+//         pointToLayer: function (feature, latlng) {
+//             return L.circleMarker(latlng, {
+
+//                 radius: feature.properties.mag * 3,
+//                 fillColor: colormap(feature.properties.mag),
+//                 color: 'black',
+//                 fillOpacity: 1,
+//                 weight: 0.6
+                
+//             })
+//         },
+//         onEachFeature: onEachFeature
+//     });
+
+//     //create quakes legend control
+//     var quakesLegend = L.control({ position: "bottomright" });
+
+//     quakesLegend.onAdd = function() {
+
+//     var geojson = L.choropleth(data, {
+//         valueProperty: "mag",
+//         scale: ["yellow", "red"],
+//         steps: 6,    
+//     });
+
+//     var div = L.DomUtil.create("div", "info legend");
+
+//     var magnitudes = ['0-1', '1-2', '2-3', '3-4', '4-5', '5+']
+
+//     var labelsColors = geojson.options.limits.map((lim, ind) => {
+//         return ('<li style="background-color: ' + geojson.options.colors[ind] + '"></li>')
+//     })
+//     var labelsLabels = geojson.options.limits.map((lim, ind) => {
+//         return ('<li>'+ magnitudes[ind]+ '</li>')
+//     })
+    
+//     div.innerHTML += "<ul>" + labelsColors.join("") + "</ul>" + "<ul>" + labelsLabels.join("") + "</ul>";
+//     return div;
+//     };
+
+//     console.log(quakesLegend);
+//     getPlates(quakesData, quakesLegend);
+// }
+
+// //create plates layer
+// function getPlates(quakes,legend) { 
+
+//     d3.json(platesUrl, function(data) {
+//       var plates = L.geoJSON(data, {
+//         style: function (feature) {
+//           return {
+//             color: 'orange',
+//             weight: 1,
+//             fillColor: 'none'
+//           };
+//         }
+//       });
+//     //   getVocanoes(quakes, legend, plates);
+//     //getWater(quakes, legend, plates);
+//     renderMap(quakes, legend, plates);
+//     });
+// };
 
 // function getVocanoes(quakes, legend, plates) { //data server not working now for some reason
 //     d3.json(volcanoesUrl, function(data) {
@@ -137,71 +278,73 @@ function getPlates(quakes,legend) {
 
 // function renderMap(quakes, legend, plates, volcs, water, wcl) {
 //function renderMap(quakes, legend, plates, water, wcl) {
-function renderMap(quakes, legend, plates, wcl) {
 
-    var satellTiles = L.tileLayer(
-        "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
-          "access_token=" + groundURL);
+
+// function renderMap(quakes, legend, plates, wcl) {
+
+//     var satellTiles = L.tileLayer(
+//         "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?" +
+//           "access_token=" + groundURL);
     
-    var regTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?' +
-    "access_token=" + groundURL);
+//     var regTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?' +
+//     "access_token=" + groundURL);
 
-    var ligthTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?' +
-    "access_token=" + groundURL)
+//     var ligthTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?' +
+//     "access_token=" + groundURL)
 
-    var baseMaps = { 
-        "Satellite": satellTiles,
-        "Street": regTiles,
-        'Greyscale': ligthTiles
-    };
+//     var baseMaps = { 
+//         "Satellite": satellTiles,
+//         "Street": regTiles,
+//         'Greyscale': ligthTiles
+//     };
     
-    var overLay = { 
-        'Quakes': quakes,
-        'Plates': plates,
-        // 'Explosions': volcs,
-        //'Water Stations': wcl,
-        //'Water Stations Heat': water
-    };
+//     var overLay = { 
+//         'Quakes': quakes,
+//         'Plates': plates,
+//         // 'Explosions': volcs,
+//         //'Water Stations': wcl,
+//         //'Water Stations Heat': water
+//     };
 
-    var myMap = L.map("map", {
-        center: [0, 0],
-        zoom: 2,
-        layers: [satellTiles, quakes],
-        timeDimension: true,
-        fullscreenControl: true,
-        timeDimensionOptions: {
-            autoPlay: true,
-            position: 'bottomleft',
-            playerOptions: {
-                transitionTime: 500,
-                loop: true,
-            },
-        },
-    });
+//     var myMap = L.map("map", {
+//         center: [0, 0],
+//         zoom: 2,
+//         layers: [satellTiles, quakes],
+//         timeDimension: true,
+//         fullscreenControl: true,
+//         timeDimensionOptions: {
+//             autoPlay: true,
+//             position: 'bottomleft',
+//             playerOptions: {
+//                 transitionTime: 500,
+//                 loop: true,
+//             },
+//         },
+//     });
 
-    L.control.layers(baseMaps, overLay, {
-        collapsed: false
-      }).addTo(myMap);
+//     L.control.layers(baseMaps, overLay, {
+//         collapsed: false
+//       }).addTo(myMap);
     
-    legend.addTo(myMap);
+//     legend.addTo(myMap);
     
-    //disappearing legend
-    myMap.on('overlayadd', function(eventLayer){
-        if (eventLayer.name === 'Quakes'){
-            myMap.addControl(legend);
-        } 
-    });
+//     //disappearing legend
+//     myMap.on('overlayadd', function(eventLayer){
+//         if (eventLayer.name === 'Quakes'){
+//             myMap.addControl(legend);
+//         } 
+//     });
     
-    myMap.on('overlayremove', function(eventLayer){
-        if (eventLayer.name === 'Quakes'){
-            myMap.removeControl(legend);
-        } 
-    });
+//     myMap.on('overlayremove', function(eventLayer){
+//         if (eventLayer.name === 'Quakes'){
+//             myMap.removeControl(legend);
+//         } 
+//     });
     
-};
+// };
 
 
-
+render()
 
 
 
